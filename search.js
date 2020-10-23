ff124549af27b458c444257a67f07608142e2a14
SultanaK <kr.shampa@yahoo.com>
2020-10-22 23:31:53 -0500

    add queues

--- search.js
+++ search.js
@@ -178,5 +178,25 @@ class BinarySearchTree {
 
         return values;
     }
+    inOrder(values = []) {
+        if (this.left) values = this.left.inOrder(values);
+        values.push(this.key);
+        if (this.right) values = this.right.inOrder(values);
+        return values;
+    }
 
+    preOrder(values = []) {
+        values.push(this.key);
+        if (this.left) values = this.left.preOrder(values);
+        if (this.right) values = this.right.preOrder(values);
+        return values;
+    }
+
+    postOrder(values = []) {
+        if (this.left) values = this.left.postOrder(values);
+        if (this.right) values = this.right.postOrder(values);
+        values.push(this.key);
+        return values;
+    }
 }
+module.exports = BinarySearchTree; 
